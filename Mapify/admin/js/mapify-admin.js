3619339b99e2b268c8805607d2d3c3bb28ae3346
talshi <talshiran0000@gmail.com>
2016-04-21 12:54:39 +0300

    update mapify files

---
+++ Mapify/admin/js/mapify-admin.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+    var wp_mapify_app = angular.module('wp_mapify_app', ['ngRoute']);
+    
+    wp_mapify_app.controller('', function($scope) {
+        
+    });
+
+    wp_mapify_app.controller('', function($scope) {
+        
+    });
+
+    wp_mapify_app.controller('', function($scope) {
+    
+    });
+
+    wp_mapify_app.config(function($routeProvider) {
+        $routeProvider
+            .when('/map', {
+                templateUrl : 'mapify-admin-display.php',
+                controller  : 'adminCtrl',
+                controllerAs: 'admin'
+            })
+            .when('/activities', {
+                templateUrl : 'mapify-activities-display.php',
+                controller  : 'activitiesCtrl',
+                controllerAs: 'activities'
+            })
+            .when('/categories', {
+                templateUrl : 'mapify-categories-display.php',
+                controller  : 'categoriesCtrl',
+                controllerAs: 'categories'
+            });
+    });
+})(jQuery);
