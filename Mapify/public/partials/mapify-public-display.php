5f34073deede99bbcd20a1ee20c3a5823aac4d47
AlumaK <aluma.ketz@gmail.com>
2016-05-27 12:16:59 +0300

    change the map image for activity by id

--- Mapify/public/partials/mapify-public-display.php
+++ Mapify/public/partials/mapify-public-display.php
@@ -23,23 +23,38 @@ function display($atts)
     
     
     
-    $content .= '<img id="tag1" class="tag" style="position: absolute; height: 50px; width: 50px; top: 20px; left: 200px;" src="http://www.snafu.org/GeoTag/GeoTagHelp/images/icon128.png" />';
+    $content .= '<img id="activity1" class="tag" style="position: absolute; height: 50px; width: 50px; top: 20px; left: 200px;" src="http://www.snafu.org/GeoTag/GeoTagHelp/images/icon128.png" />';
+    
+    $content .= '<img id="activity2" class="tag" style="position: absolute; height: 50px; width: 50px; top: 50px; left: 300px;" src="http://www.snafu.org/GeoTag/GeoTagHelp/images/icon128.png" />';
     
 //<img id="tag2" class="tag" style="position: absolute; height: 50px; width: 50px; top: 0px; left: 0px;" src="http://www.snafu.org/GeoTag/GeoTagHelp/images/icon128.png" alt="" onmouseover = function(){#map.css(background-image: url("https://www.jerusalem.muni.il/Municipality/Directorquarters/PublishingImages/map.jpg"););} />';
 
     //$content .= '<div id="a"/>';
     
     
-    $content .= '</div>';
+    
     
     $content .= "\n<script>
-        jQuery('#tag1').hover(
+            function getImageByActivity(activity)
+        {
+            if(activity == 'activity1')
+                return 'http://www.jiis.org.il/.upload/publications/images/2007_07_minhalim-map-small.jpg';
+            else
+                return 'http://jiis.org/.upload/heb/data_statistics/maps/flat_size/2010_02_flatsize-map.jpg';
+
+        }
+        jQuery('.tag').hover(
         function(){
-        jQuery('#map').css('background-image', " . 
-        "'url(" . '"http://www.jiis.org.il/.upload/publications/images/2007_07_minhalim-map-small.jpg"' . ")');
+           
+        var activityId = jQuery(this).attr('id');
+        var urlByActivityId = getImageByActivity(activityId);
+        //console.log(urlByActivityId);
+        jQuery('#map').css('background-image','url(' + urlByActivityId + ')');
         });
     </script>";
-
+    
+    
+$content .= '</div>';
     return $content;
 }
 
@@ -50,7 +65,7 @@ add_shortcode('custom-mapify', 'display');
 
 
 
-
+/*
 function getImgURL()    //for example
 {
     return "http://static.guim.co.uk/sys-images/Guardian/Pix/maps_and_graphs/2010/5/20/1274374118960/Graphic-map---museum-of-m-001.jpg";
@@ -69,7 +84,7 @@ function changeForMap2()
 {
     
 }
-
+*/
 ?>
 
 
