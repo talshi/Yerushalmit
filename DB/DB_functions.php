995d1f3f1f57a26641a50914f830cddf8d4b267c
stavdv <stavbarazani@gmail.com>
2016-05-29 12:21:07 +0300

    apdate DB_functions file

--- DB/DB_functions.php
+++ DB/DB_functions.php
@@ -1,24 +1,22 @@
 <?php
 
 public static function get_category_list_by_id($id)
-	{
-		//option 1
-		/*$results = $wpdb->get_results( "SELECT * FROM wp_categories WHERE option_id = '$id'", OBJECT );*/
-		
-		//option 2
-		$results = $GLOBALS['wpdb']->get_results( 'SELECT * FROM wp_categories WHERE id = 1', OBJECT );
-		//print_r($results);
+{
+		//select sql 
+		$sqlResults = $GLOBALS['wpdb']->get_results( "SELECT * FROM wp_categories WHERE id = '$id'", OBJECT );
 		
 		//convert to json and return the array
-		return json_encode($results, JSON_PRETTY_PRINT);
-	}
+		return json_encode($sqlResults,JSON_PRETTY_PRINT);
+}
 	
-	/*write in activetor class:
-		$json = Tables::get_category_list_by_id();
+
+/*to check get_category_list_by_id function: write in activetor class:
+
+		$json = Tables::get_category_list_by_id(any number we wont to delete);
 		print_r($json);
-	*/
-	
-}	
+		
+		Output expected: A specific line with all the details, according id.
+*/	
 
 delete_category($category)
 {
@@ -27,10 +25,9 @@ delete_category($category)
 
 get_category_list()
 {
-
 }
-
 // Integration with php code instead of return value in functions
 /*<script type="text/javascript">
 	var category = <?php echo json_encode($results, JSON_PRETTY_PRINT) ?>;
 </script>*/
+
