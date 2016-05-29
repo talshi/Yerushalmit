b0dd09bdc5ce3c2003e1f7af6fc24dd0ce0247e0
Guy <guyworkprg@gmail.com>
2016-05-29 21:10:03 +0300

    @talshi new window with table to insert new activity

--- Mapify/admin/partials/mapify-categories-display.php
+++ Mapify/admin/partials/mapify-categories-display.php
@@ -30,7 +30,6 @@
 		<input	type="button" value="Delete All" id="IDdeleteAllCategory" />
 	</div>
 
-
 	<div id="myModal" class="modal fade" role="dialog">
 		<div class="modal-dialog">
 			<!-- Modal content-->
@@ -38,9 +37,76 @@
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 					<h4 class="modal-title">Add New Catergory</h4>
+									<form class="modal-body" role="form" ng-submit="addActivity()">
+					
+				<div id = "IDinputDataCategory" >
+				
+					<table id = "IdInsertTableCategory">
+						<tr>
+							<td><label>Category Name </label></td>
+							<td><input type="text" ng-model="CategoryName"></td>
+						</tr>
+						<tr>
+							<td><label>Description </label></td>
+							<br><br>
+							<td><textarea type="text" ng-model="CategoryyDescription"  id="IDareaText"> </textarea></td>
+						</tr>
+					</table>
+				
+				</div>
+					
+					<div id="upload_image_admin">
+						<div id="upload_note"> Enter an URL or upload an category image </div>
+						<div id="upload_image_container">
+
+							<label id="upload_map_label" for="upload_image">Upload Image</label>
+							<input id="upload_image" type="text" size="36"
+								name="upload_image" value="" /> <input id="upload_image_button"
+								type="button" value="Upload Image" /> <input id="save_button"
+								type="button" value="Save Image" /> <br />
+							</td>
+						</div>
+					</div>
+
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<!-- add item to DB.... -->
+						<button id="save-button" class="btn btn-default" type="submit"
+							action="">Save</button>
+					</div>
+
+				</form>
+					
 				</div>
 			</div>
 		</div>
 	</div>
 	
 </div>
+
+
+
+<script type="text/javascript">
+    jQuery(document).ready(function ($) {
+        $("#upload_image_button").click(function (e) {
+            e.preventDefault();
+            var image = wp.media({
+                title: 'Upload Image',
+                multiple: false
+            }).open()
+            .on('select', function (e) {
+                // This will return the selected image from the Media Uploader, the result is an object
+                var uploaded_image = image.state().get('selection').first();
+                // We convert uploaded_image to a JSON object to make accessing it easier
+                // Output to the console uploaded_image
+                //console.log(uploaded_image);
+                var image_url = uploaded_image.toJSON().url;
+                // Let's assign the url value to the input field
+                $('#upload_image').val(image_url)
+                var image_link = $('#upload_image').val();
+                $("#preview_label").html("Preview:");
+                $("#img_preview").attr("src", image_link);
+            });
+        });
+    });
+</script>
