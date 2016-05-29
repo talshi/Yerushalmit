0e2a1e9fe5e90eff2c5265023fcbe2de1b3c7216
Guy <guyworkprg@gmail.com>
2016-05-29 20:30:45 +0300

    @talshi add 3 button in Category Area - admin

--- Mapify/admin/partials/mapify-categories-display.php
+++ Mapify/admin/partials/mapify-categories-display.php
@@ -1,24 +1,34 @@
 
 <h1>Manage Categories</h1>
-<div class="note">Organize your categories.</div><br>
+<div class="note">Organize your categories.</div>
+<br>
 
-	<label for="search">Search: </label> <input name="search" type="text" ng-model="query" />
-	<!-- search bar -->
-	
+<label for="search">Search: </label>
+<input name="search" type="text" ng-model="query" />
+<!-- search bar -->
+
 <div class="activities-table" ng-controller="activitiesCtrl">
 
-		<table>
-			<tr>
-				<th>#</th>
-				<th id ="IDcategory-name" ng-model="name" ng-click="sortBy='name'; reverseSort=!reverseSort">Category Name</th>
-				<th>Description</th>
-			</tr>
-						
-			<tr ng-repeat="category in categories_list | filter: query | orderBy:sortBy:reverseSort"  id="table" >
-				<td><input type="checkbox" /></td>
-				<td>{{ category.name }}</td>
-				<td>{{ category.description }}</td>
-			</tr>
-		</table>
-		
+	<table>
+		<tr>
+			<th>#</th>
+			<th id="IDcategory-name" ng-model="name"
+				ng-click="sortBy='name'; reverseSort=!reverseSort">Category Name</th>
+			<th>Description</th>
+		</tr>
+
+		<tr
+			ng-repeat="category in categories_list | filter: query | orderBy:sortBy:reverseSort"
+			id="table">
+			<td><input type="checkbox" /></td>
+			<td>{{ category.name }}</td>
+			<td>{{ category.description }}</td>
+		</tr>
+	</table>
+	<div>
+		<input type="button" value="Add new Category" id="IDnewCategory" /> <input
+			type="button" value="remove selected" id="IDdeleteSelectedCategory" /> <input
+			type="button" value="Delete All" id="IDdeleteAllCategory" />
+
 	</div>
+</div>
